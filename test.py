da2163813b5e7ea95b967ebc7931e2dce4cd0c2b
Andrew Brown <andrewb1230@gmail.com>
2020-05-14 22:50:11 -0700

    day 1 done

---
+++ test.py
@@ -0,0 +1,8 @@
+def mean(value):
+    if type(value) == dict:
+        the_mean = sum(value.values()) / len(value)
+    else:
+        the_mean = sum(value) / len(value)
+    
+    return the_mean
+print(mean([1,4,8]))
