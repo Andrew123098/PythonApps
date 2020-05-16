1db7159d444950d27fb3bc46302fd299cd1a2c4f
Andrew Brown <andrewb1230@gmail.com>
2020-05-15 21:13:37 -0700

    day 2 done

--- test.py
+++ test.py
@@ -1,8 +1,5 @@
-def mean(value):
-    if type(value) == dict:
-        the_mean = sum(value.values()) / len(value)
-    else:
-        the_mean = sum(value) / len(value)
-    
-    return the_mean
-print(mean([1,4,8]))
+with open("fruits.txt") as myfile:
+    content = myfile.read()
+
+print(content)
+print(content)
