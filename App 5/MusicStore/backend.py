a282b3df4ba10f4c875f3425616f589fbde7c5e3
Andrew Brown <andrewb1230@gmail.com>
2020-06-08 13:10:41 -0700

    Finished App 5 comments

--- App 5/MusicStore/backend.py
+++ App 5/MusicStore/backend.py
@@ -1,45 +1,55 @@
 import sqlite3
 
+##############
+## BACK END ##
+##############
+
+## Connect to the database and connect/create the music table. 
 def connect():
-    conn=sqlite3.connect("music.db")
-    cur=conn.cursor()
+    conn=sqlite3.connect("music.db") # Connect to database file.
+    cur=conn.cursor()                # Create cursor object to run through database.
     cur.execute("CREATE TABLE IF NOT EXISTS music (id INTEGER PRIMARY KEY, title text, artist text, album text, year integer)")
-    conn.commit()
-    conn.close()
+    conn.commit()                    # Commit changes to database.
+    conn.close()                     # Close connection to database.
 
+## Add song to music database
 def insert(title, artist, album, year):
-    conn=sqlite3.connect("music.db")
-    cur=conn.cursor()
+    conn=sqlite3.connect("music.db") # Connect to database file.
+    cur=conn.cursor()                # Create cursor object to run through database.
     cur.execute("INSERT INTO music VALUES (NULL,?,?,?,?)", (title, artist, album, year))
-    conn.commit()
-    conn.close()
+    conn.commit()                    # Commit chnages to database.
+    conn.close()                     # Close connection to database.
 
+## View all songs in the music library.
 def view():
-    conn=sqlite3.connect("music.db")
-    cur=conn.cursor()
-    cur.execute("SELECT * FROM music")
-    rows=cur.fetchall()
-    conn.close()
-    return rows
+    conn=sqlite3.connect("music.db")   # Connect to database file.
+    cur=conn.cursor()                  # Create cursor object to run through database.
+    cur.execute("SELECT * FROM music") # Select all data from music table in database.
+    rows=cur.fetchall()                # Set variable to store all selected data.
+    conn.close()                       # Close connection to database (no chnages made).
+    return rows                        # Return all data from music table.
 
-def search(title="", artist="", album="", year=""):
-    conn=sqlite3.connect("music.db")
-    cur=conn.cursor()
+## Seach for a song using one or more of the 4 information entries.
+def search(title="", artist="", album="", year=""): # Initialize variables to be empty strings.
+    conn=sqlite3.connect("music.db")                # Connect to the database file.
+    cur=conn.cursor()                               # Create cursor object to run through database.
     cur.execute("SELECT * FROM music WHERE title=? OR artist=? OR album=? OR year=?", (title, artist, album, year))
-    rows=cur.fetchall()
-    conn.close()
-    return rows
+    rows=cur.fetchall()                             # Set variable to store all selected data.
+    conn.close()                                    # Close connection to database (no chnages made).
+    return rows                                     # Return all songs that match the search term.
 
-def delete(id):
-    conn=sqlite3.connect("music.db")
-    cur=conn.cursor()
+## Delete a song from the music library.
+def delete(id): 
+    conn=sqlite3.connect("music.db") # Connect to the database file.
+    cur=conn.cursor()                # Create cursor object to run through database.
     cur.execute("DELETE FROM music WHERE id=?", (id,))
-    conn.commit()
-    conn.close()
+    conn.commit()                    # Commit chnages to database.
+    conn.close()                     # Close connection to database.
 
+## Update a song in the music library.
 def update(id,title,artist,album,year):
-    conn=sqlite3.connect("music.db")
-    cur=conn.cursor()
+    conn=sqlite3.connect("music.db") # Connect to the database file.
+    cur=conn.cursor()                # Create cursor object to run through database.
     cur.execute("UPDATE music SET title=?, artist=?, album=?, year=? WHERE id=?", (title, artist, album, year, id))
-    conn.commit()
-    conn.close()
+    conn.commit()                    # Commit chnages to database.
+    conn.close()                     # Close connection to database.
--- App 5/MusicStore/frontend.py
+++ App 5/MusicStore/frontend.py
@@ -1,4 +1,4 @@
-# Andrew Brown : Last Edited 6/6/2020
+# Andrew Brown : Last Edited 6/8/2020
 # This program stores song information:
 # Title, Artist, Album, Year
 #
@@ -9,42 +9,48 @@
 # Add an entry
 # Update an entry
 # Delete an entry
-# Close
+# Clear all entries
 
 ###############
 ## FRONT END ##
 ###############
 
-from tkinter import *
-import backend
+from tkinter import *   # Import tkinter w/o need for tkinter prefix.
+import backend          # Import functions from backend file.
 
+## View all songs in the music library.
 def view_command():
-    list1.delete(0,END)
-    for row in backend.view():
-        list1.insert(END, row)
+    list1.delete(0,END)        # Clear the list box.
+    for row in backend.view(): # Loop through all rows returned by view function.
+        list1.insert(END, row) # Insert each row as a different row in the list box.
 
+## Seach for a song using one or more of the 4 information entries.
 def search_command():
-    list1.delete(0,END)
+    list1.delete(0,END)         # Clear the list box. >> Loop through tuple returned by search function.
     for row in backend.search(title_text.get(), artist_text.get(), album_text.get(), year_text.get()):
-        list1.insert(END, row)
+        list1.insert(END, row)  # Insert each tuple in a row in the list box.
 
+## Add song to music database
 def add_command():
-    backend.insert(title_text.get(), artist_text.get(), album_text.get(), year_text.get())
-    list1.delete(0,END)
-    list1.insert(END,(title_text.get(), artist_text.get(), album_text.get(), year_text.get()))
+    backend.insert(title_text.get(), artist_text.get(), album_text.get(), year_text.get())     # Insert values from entry boxes into music library
+    list1.delete(0,END)                                                                        # Clear the list box.
+    list1.insert(END,(title_text.get(), artist_text.get(), album_text.get(), year_text.get())) # Insert values from entry boxes into list box.
 
-def update_command():
-    backend.update(selected_tuple[0],title_text.get(), artist_text.get(), album_text.get(), year_text.get())
+## Update a song in the music library
+def update_command(): ## >> Call backend update function with selected id and new values from the entry boxes.
+    backend.update(selected_tuple[0],title_text.get(), artist_text.get(), album_text.get(), year_text.get()) 
 
+## Delete a song from the music library
 def delete_command():
-    backend.delete(selected_tuple[0])
+    backend.delete(selected_tuple[0]) # Delete selected song from the database using song id
 
+## Get tuple of selected row when user clicks on song in list box.
 def get_selected_row(event):
-    try:
-        global selected_tuple
-        index=list1.curselection()[0]
-        selected_tuple=list1.get(index)
-        e1.delete(0,END)
+    try:                                  
+        global selected_tuple             # Save song tuple as a global variable
+        index=list1.curselection()[0]     # Store song ID of selected song in index variable
+        selected_tuple=list1.get(index)   # Store all song info in the tuple global variable
+        e1.delete(0,END)                  ### Clear all the entry boxes and insert info from selected song
         e1.insert(END,selected_tuple[1])
         e2.delete(0,END)
         e2.insert(END,selected_tuple[2])
@@ -52,20 +58,25 @@ def get_selected_row(event):
         e3.insert(END,selected_tuple[3])
         e4.delete(0,END)
         e4.insert(END,selected_tuple[4])
-    except IndexError:
-        pass
+    except IndexError:                    # If the user clicks in the listbox when there are no songs in the database (IndexError)
+        pass                              # Do nothing
 
+## Clear the entry boxes.
 def clear_command():
     e1.delete(0,END)
     e2.delete(0,END)
     e3.delete(0,END)
     e4.delete(0,END)
 
-window=Tk()   # Create the window that will hold all of our objects
+window=Tk() # Create the window that will hold all of our objects (All frontend GUI design goes b/w here and window.mainloop())
 
-window.wm_title("Music Library")
+window.wm_title("Music Library") # Titles the Window
 
-backend.connect()
+#################################
+## LABELS:                     ##
+## Create label in the window. ##
+## Place lable on GUI grid.    ##
+#################################
 
 l1=Label(window,text="Title")
 l1.grid(row=0,column=0)
@@ -78,6 +89,13 @@ l3.grid(row=0,column=2)
 
 l4=Label(window,text="Year")
 l4.grid(row=1,column=2)
+
+###########################################################
+## ENTRY BOXES:                                          ##
+## Initialize storage variables as string variables      ##
+## Create entry boxes in the window w/ storage variables ##
+## Place entry boxes on GUI grid.                        ##
+###########################################################
 
 title_text=StringVar()
 e1=Entry(window,textvariable=title_text)
@@ -95,22 +113,42 @@ year_text=StringVar()
 e4=Entry(window,textvariable=year_text)
 e4.grid(row=1,column=3)
 
+##########################################################
+## LIST BOX:                                            ##
+## Create list box in window and define dimensions.     ##
+## Define list box placement and how many RxC it spans. ## 
+##########################################################
+
 list1=Listbox(window,height=6,width=35)
 list1.grid(row=2,column=0, rowspan=6, columnspan=2)
 
+##############################################################
+## SCROLLBARS:                                              ##
+## Create scrollbar in window.                              ##
+## Place scrollbar on window grid.                          ##
+## Set scroll bar to control y-axis scrolling of list1.     ##
+## Set list one to take y-axis commands from the scrollbar. ##
+##############################################################
+
 sb1=Scrollbar(window)
 sb1.grid(row=2,column=2,rowspan=6)
-
-sb2=Scrollbar(window, orient='horizontal')
-sb2.grid(row=7,column=0,columnspan=2)
-
-list1.configure(xscrollcommand=sb2.set)
-sb2.configure(command=list1.xview)
-
 list1.configure(yscrollcommand=sb1.set)
 sb1.configure(command=list1.yview)
 
+sb2=Scrollbar(window, orient='horizontal') # Orients the scrollbar sideways in the GUI
+sb2.grid(row=7,column=0,columnspan=2)
+list1.configure(xscrollcommand=sb2.set)
+sb2.configure(command=list1.xview)
+
+## When I click on an item in the listbox, run the function get_selected_row.
 list1.bind('<<ListboxSelect>>', get_selected_row)
+
+#########################################################
+## BUTTONS:                                            ##
+## Create buttons in window with defined label, width, ##
+## and funtion executed when pressed.                  ##
+## Place button on GUI grid.                           ##
+#########################################################
 
 b1=Button(window,text="View All",width=12, command=view_command)
 b1.grid(row=2,column=3)
